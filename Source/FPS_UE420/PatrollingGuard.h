e43179c5483243ea93a1b11cbe3891fcf5855082
dannyliu15 <a46949404@outlook.com>
2018-08-21 16:02:56 +0800

    add PatrollingGuard class inheritted from TP_ThirdPersonCharactor

---
+++ Source/FPS_UE420/PatrollingGuard.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Runtime/Engine/Classes/GameFramework/Actor.h"
+#include "TP_ThirdPerson/TP_ThirdPersonCharacter.h"
+#include "PatrollingGuard.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPS_UE420_API APatrollingGuard : public ATP_ThirdPersonCharacter
+{
+	GENERATED_BODY()
+	
+protected:
+	UPROPERTY(EditInstanceOnly, Category = "Patroll Points of this instance")
+	TArray<AActor*> PatrollPoints;
+	
+	
+};
